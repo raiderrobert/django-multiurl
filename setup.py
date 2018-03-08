b7f739f21c83f3b9160fd74df15f3db5a76ba5a1
rroskam <rroskam@worthwhile.com>
2018-03-08 15:04:38 -0500

    Setting up 1.2.0 for release

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='django-multiurl',
     py_modules=['multiurl'],
-    version='1.1.0',
+    version='1.2.0',
     description='Allow multiple views to match the same URL.',
     license='BSD',
     url='https://github.com/raiderrobert/django-multiurl',
-    download_url='https://github.com/raiderrobert/django-multiurl/tarball/v1.1.0',
+    download_url='https://github.com/raiderrobert/django-multiurl/tarball/v1.2.0',
     author='Jacob Kaplan-Moss and Robert Roskam',
     author_email='raiderrobert@gmail.com',
-    install_requires=['django>=1.5'],
+    install_requires=['django>=1.11'],
     keywords='django urls',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -21,7 +21,8 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
